eced596162c0b7fd3d7c8814a3daef9cb198cbdc
Rubit-IO <42852853+Rubit-IO@users.noreply.github.com>
2018-09-13 19:33:18 -0500

    Update basics.h

--- 01-basics/basics.h
+++ 01-basics/basics.h
@@ -3,5 +3,4 @@
 //write function prototype for multiply_numbers with two integer parameters
 int multiply_numbers(int num1, int num2);
 
-void pass_by_val_by_ref(int val, int& ref) {
-}
+void pass_by_val_by_ref(int val, int& ref);
